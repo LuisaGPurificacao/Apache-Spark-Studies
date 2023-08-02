2fccfb18bc4c4544662f9efe7f6a345a4320d45e
luisagpurificacao <luisa.purificacao16@gmail.com>
2023-08-02 12:37:46 -0300

    Spark SQL

---
+++ HelloSparkSQL/HelloSparkSQL.py
@@ -0,0 +1,27 @@
+import sys
+
+from pyspark.sql import SparkSession
+
+from lib.logger import Log4J
+
+if __name__ == "__main__":
+    spark = SparkSession \
+        .builder \
+        .master("local[3]") \
+        .appName("HelloSparkSQL") \
+        .getOrCreate()
+
+    logger = Log4J(spark)
+
+    if len(sys.argv) != 2:
+        logger.error("Usage: HelloSpark <filename>")
+        sys.exit(-1)
+
+    surveyDF = spark.read \
+        .option("header", "true") \
+        .option("inferSchema", "true") \
+        .csv(sys.argv[1])
+
+    surveyDF.createOrReplaceTempView("survey_tbl")
+    countDF = spark.sql("select Country, count(1) as count from survey_tbl where Age<40 group by Country")
+    countDF.show()
