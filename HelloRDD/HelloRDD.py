1970a4b98a6b1d1147fcf497f95c8e0a2b75fcce
luisagpurificacao <luisa.purificacao16@gmail.com>
2023-08-02 12:16:10 -0300

    Spark RDD

---
+++ HelloRDD/HelloRDD.py
@@ -0,0 +1,43 @@
+import sys
+from collections import namedtuple
+
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+
+from lib.logger import Log4J
+
+SurveyRecord = namedtuple("SurveyRecord", ["Age", "Gender", "Country", "State"])
+if __name__ == "__main__":
+    conf = SparkConf() \
+        .setMaster("local[3]") \
+        .setAppName("HelloRDD")
+
+    spark = SparkSession \
+        .builder \
+        .config(conf=conf) \
+        .getOrCreate()
+
+    sc = spark.sparkContext
+    logger = Log4J(spark)
+
+    if len(sys.argv) != 2:
+        logger.error("Usage: HelloSpark <filename>")
+        sys.exit(-1)
+
+    linesRDD = sc.textFile(sys.argv[1])
+    partitionedRDD = linesRDD.repartition(2)
+
+    colsRDD = partitionedRDD.map(lambda line: line.replace('"', '').split(","))
+
+    selectRDD = colsRDD.map(lambda cols: SurveyRecord(int(cols[1]), cols[2], cols[3], cols[4]))
+
+    filteredRDD = selectRDD.filter(lambda r: r.Age < 40)
+
+    kvRDD = filteredRDD.map(lambda r: (r.Country, 1))
+
+    countRDD = kvRDD.reduceByKey(lambda v1, v2: v1 + v2)
+
+    colsList = countRDD.collect()
+
+    for x in colsList:
+        logger.info(x)
